e14909ae8a04d78ab38b7afd2076165bf366a828
Tito Yslas <roberto.yslas@gmail.com>
2017-10-18 14:42:57 -0600

    ran seeds

--- seeds/user_secrets.js
+++ seeds/user_secrets.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('user_secret').insert([
-        {id: 1, user_id: 1, seccret_id: 1}
+        {id: 1, user_id: 1, secret_id: 1}
       ]);
     });
 };
