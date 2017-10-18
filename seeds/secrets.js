a000f876c7c091a0d8b8871a56304bf8c5535a89
Tito Yslas <roberto.yslas@gmail.com>
2017-10-18 14:07:11 -0600

    hard coded the seeds

--- seeds/secrets.js
+++ seeds/secrets.js
@@ -1,13 +1,11 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('secret').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('secret').insert([
+        {id: 1, secret: 'I like to bob for apples in the toilet (sandlot)'}
       ]);
     });
 };
--- seeds/user_secrets.js
+++ seeds/user_secrets.js
@@ -1,13 +1,11 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('user_secret').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('user_secret').insert([
+        {id: 1, user_id: 1, seccret_id: 1}
       ]);
     });
 };
